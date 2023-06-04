f739dcb0590efc313b77a4a110ccb29aa423b1ad
gooodwin67 <sn67@inbox.ru>
2023-06-04 14:49:41 +0300

    .

--- lib/screens/login_screen/login_pass_enter_screen.dart
+++ lib/screens/login_screen/login_pass_enter_screen.dart
@@ -15,62 +15,94 @@ class LoginPassEnterScreen extends StatelessWidget {
 
     return Scaffold(
       backgroundColor: mainColor,
-      body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.all(mainPadding),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Stack(
-                alignment: Alignment.centerLeft,
-                children: [
-                  InkWell(
-                    onTap: () => context.go('/'),
-                    child: const Icon(
-                      Icons.chevron_left,
-                      color: Colors.white,
-                      size: 30,
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text('Жалко выкинуть',
-                          style: Theme.of(context)
-                              .textTheme
-                              .titleLarge
-                              ?.copyWith(color: Colors.white)),
-                    ],
-                  ),
-                ],
-              ),
-              SizedBox(height: 40),
-              Container(
-                margin: EdgeInsets.symmetric(horizontal: 30),
-                padding: EdgeInsets.symmetric(horizontal: 15, vertical: 20),
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(11),
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.all(mainPadding),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Stack(
+                  alignment: Alignment.centerLeft,
                   children: [
-                    Container(
-                      height: 43,
-                      child: TextField(
+                    InkWell(
+                      onTap: () => context.go('/'),
+                      child: const Icon(
+                        Icons.chevron_left,
+                        color: Colors.white,
+                        size: 30,
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text('Жалко выкинуть',
+                            style: Theme.of(context)
+                                .textTheme
+                                .titleLarge
+                                ?.copyWith(color: Colors.white)),
+                      ],
+                    ),
+                  ],
+                ),
+                SizedBox(height: 40),
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 30),
+                  padding: EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(11),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                        height: 43,
+                        child: TextField(
+                          onChanged: ((value) => context
+                              .read<LoginScreenProvider>()
+                              .editLogin(value)),
+                          style: Theme.of(context).textTheme.bodyText1,
+                          decoration: InputDecoration(
+                            floatingLabelBehavior: FloatingLabelBehavior.never,
+                            contentPadding: EdgeInsets.zero,
+                            filled: true,
+                            fillColor: Color(0xffF2F0F7),
+                            prefixIcon: Icon(Icons.account_circle_outlined),
+                            label: Text(
+                              'Email',
+                              style: Theme.of(context).textTheme.bodyText1,
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(50),
+                              borderSide: BorderSide(
+                                  color: Colors.transparent, width: 0),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(50),
+                              borderSide: BorderSide(
+                                  color: Colors.transparent, width: 0),
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      TextField(
                         onChanged: ((value) => context
                             .read<LoginScreenProvider>()
-                            .editLogin(value)),
+                            .editPassword(value)),
+                        obscureText: true,
                         style: Theme.of(context).textTheme.bodyText1,
                         decoration: InputDecoration(
                           floatingLabelBehavior: FloatingLabelBehavior.never,
                           contentPadding: EdgeInsets.zero,
                           filled: true,
                           fillColor: Color(0xffF2F0F7),
-                          prefixIcon: Icon(Icons.account_circle_outlined),
+                          prefixIcon: Icon(Icons.lock_outlined),
+                          suffixIcon: Icon(Icons.remove_red_eye_outlined),
                           label: Text(
-                            'Email',
+                            'Пароль',
                             style: Theme.of(context).textTheme.bodyText1,
                           ),
                           enabledBorder: OutlineInputBorder(
@@ -85,115 +117,89 @@ class LoginPassEnterScreen extends StatelessWidget {
                           ),
                         ),
                       ),
-                    ),
-                    SizedBox(height: 10),
-                    TextField(
-                      onChanged: ((value) => context
-                          .read<LoginScreenProvider>()
-                          .editPassword(value)),
-                      obscureText: true,
-                      style: Theme.of(context).textTheme.bodyText1,
-                      decoration: InputDecoration(
-                        floatingLabelBehavior: FloatingLabelBehavior.never,
-                        contentPadding: EdgeInsets.zero,
-                        filled: true,
-                        fillColor: Color(0xffF2F0F7),
-                        prefixIcon: Icon(Icons.lock_outlined),
-                        suffixIcon: Icon(Icons.remove_red_eye_outlined),
-                        label: Text(
-                          'Пароль',
-                          style: Theme.of(context).textTheme.bodyText1,
-                        ),
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(50),
-                          borderSide:
-                              BorderSide(color: Colors.transparent, width: 0),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(50),
-                          borderSide:
-                              BorderSide(color: Colors.transparent, width: 0),
-                        ),
+                      isLoginError == 1
+                          ? const Text(
+                              'Пользователя с таким Email не существует',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(color: Colors.red, fontSize: 13),
+                            )
+                          : SizedBox(),
+                      isLoginError == 2
+                          ? const Text(
+                              'Неверный пароль',
+                              style: TextStyle(color: Colors.red, fontSize: 13),
+                            )
+                          : SizedBox(),
+                      isLoginError == 3
+                          ? const Text(
+                              'Неверный формат Email',
+                              style: TextStyle(color: Colors.red, fontSize: 13),
+                            )
+                          : SizedBox(),
+                      isLoginError == 4
+                          ? const Text(
+                              'Введите данные',
+                              style: TextStyle(color: Colors.red, fontSize: 13),
+                            )
+                          : SizedBox(),
+                      SizedBox(height: 15),
+                      const InkWell(
+                          child: Text(
+                        'Забыли пароль?',
+                        style: TextStyle(
+                            color: Colors.grey,
+                            decoration: TextDecoration.underline),
+                      )),
+                      SizedBox(height: 15),
+                      ElevatedButton(
+                        onPressed: loginingNow == false
+                            ? () async {
+                                await context
+                                    .read<LoginScreenProvider>()
+                                    .tryLogin();
+                                context
+                                        .read<LoginScreenProvider>()
+                                        .isLoginCorrect
+                                    ? context.go('/main')
+                                    : null;
+                              }
+                            : () {},
+                        style: ButtonStyle(
+                            backgroundColor:
+                                MaterialStateProperty.all(mainColor),
+                            shape: MaterialStateProperty.all(
+                                RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(50),
+                            ))),
+                        child: Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 20),
+                            child: loginingNow == false
+                                ? const Text(
+                                    'Войти',
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold),
+                                  )
+                                : const Text(
+                                    'Войти',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.grey),
+                                  )),
                       ),
-                    ),
-                    isLoginError == 1
-                        ? const Text(
-                            'Пользователя с таким Email не существует',
-                            textAlign: TextAlign.center,
-                            style: TextStyle(color: Colors.red, fontSize: 13),
-                          )
-                        : SizedBox(),
-                    isLoginError == 2
-                        ? const Text(
-                            'Неверный пароль',
-                            style: TextStyle(color: Colors.red, fontSize: 13),
-                          )
-                        : SizedBox(),
-                    isLoginError == 3
-                        ? const Text(
-                            'Неверный формат Email',
-                            style: TextStyle(color: Colors.red, fontSize: 13),
-                          )
-                        : SizedBox(),
-                    isLoginError == 4
-                        ? const Text(
-                            'Введите данные',
-                            style: TextStyle(color: Colors.red, fontSize: 13),
-                          )
-                        : SizedBox(),
-                    SizedBox(height: 15),
-                    const InkWell(
-                        child: Text(
-                      'Забыли пароль?',
-                      style: TextStyle(
-                          color: Colors.grey,
-                          decoration: TextDecoration.underline),
-                    )),
-                    SizedBox(height: 15),
-                    ElevatedButton(
-                      onPressed: loginingNow == false
-                          ? () async {
-                              await context
-                                  .read<LoginScreenProvider>()
-                                  .tryLogin();
-                              context.read<LoginScreenProvider>().isLoginCorrect
-                                  ? context.go('/main')
-                                  : null;
-                            }
-                          : () {},
-                      style: ButtonStyle(
-                          backgroundColor: MaterialStateProperty.all(mainColor),
-                          shape:
-                              MaterialStateProperty.all(RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(50),
-                          ))),
-                      child: Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 20),
-                          child: loginingNow == false
-                              ? const Text(
-                                  'Войти',
-                                  style: TextStyle(fontWeight: FontWeight.bold),
-                                )
-                              : const Text(
-                                  'Войти',
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.grey),
-                                )),
-                    ),
-                    SizedBox(height: 10),
-                    InkWell(
-                        onTap: () => context.go('/registration'),
-                        child: Text(
-                          'Зарегистрироваться?',
-                          style: TextStyle(
-                              color: mainColor,
-                              decoration: TextDecoration.underline),
-                        )),
-                  ],
+                      SizedBox(height: 10),
+                      InkWell(
+                          onTap: () => context.go('/registration'),
+                          child: Text(
+                            'Зарегистрироваться?',
+                            style: TextStyle(
+                                color: mainColor,
+                                decoration: TextDecoration.underline),
+                          )),
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
