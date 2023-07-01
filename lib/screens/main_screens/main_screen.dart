17ac3885455c7dff96f41ccda07b35d356034de0
Light Course <sn67@inbox.ru>
2023-07-01 13:14:53 +0300

    refresh main screen

--- lib/screens/main_screens/main_screen.dart
+++ lib/screens/main_screens/main_screen.dart
@@ -32,139 +32,148 @@ class _MainScreenWidgetState extends State<MainScreenWidget> {
       body: SafeArea(
         child: WillPopScope(
           onWillPop: () => showExitPopup(context),
-          child: CustomScrollView(
-            slivers: [
-              SliverAppBar(
-                elevation: 0,
-                floating: true,
-                pinned: false,
-                snap: false,
-                titleSpacing: 0,
-                backgroundColor: Colors.white,
-                automaticallyImplyLeading: false,
-                expandedHeight: 59,
-                title: Padding(
-                  padding: EdgeInsets.all(mainPadding),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      InkWell(
-                        onTap: () {},
-                        child: const Icon(
-                          Icons.search,
-                          color: Colors.grey,
-                        ),
-                      ),
-                      Text(
-                        'Жалко выкинуть',
-                        style: Theme.of(context)
-                            .textTheme
-                            .titleLarge!
-                            .copyWith(fontSize: 25),
-                      ),
-                      InkWell(
-                        onTap: () {
-                          context.go('/main/account');
-                        },
-                        child: const Icon(
-                          Icons.manage_accounts_outlined,
-                          color: Colors.grey,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              SliverPadding(
-                padding: const EdgeInsets.only(bottom: 10),
-                sliver: SliverAppBar(
-                  toolbarHeight: 120,
+          child: RefreshIndicator(
+            onRefresh: () => context
+                .read<MainScreenProvider>()
+                .getAllDb(context.read<MainScreenProvider>().activeCategory),
+            child: CustomScrollView(
+              slivers: [
+                SliverAppBar(
                   elevation: 0,
                   floating: true,
                   pinned: false,
-                  snap: true,
+                  snap: false,
+                  titleSpacing: 0,
                   backgroundColor: Colors.white,
                   automaticallyImplyLeading: false,
-                  titleSpacing: 0,
-                  title: Container(
-                    height: 100,
-                    child: ListView.builder(
-                      scrollDirection: Axis.horizontal,
-                      itemCount:
-                          context.read<MainScreenProvider>().Categories.length,
-                      itemBuilder: (context, index) {
-                        return InkWell(
-                          onTap: () {
-                            context.read<MainScreenProvider>().getAllDb(context
-                                .read<MainScreenProvider>()
-                                .Categories[index]
-                                .numCategory);
-
-                            context
-                                .read<MainScreenProvider>()
-                                .changeName(index);
-                            setState(() {});
-                          },
-                          child: CategoryBlock(
-                            index: context
-                                .read<MainScreenProvider>()
-                                .activeCategory,
-                            numCat: context
-                                .read<MainScreenProvider>()
-                                .Categories[index]
-                                .numCategory,
-                            name: context
-                                .read<MainScreenProvider>()
-                                .Categories[index]
-                                .nameCategory,
-                            icon: context
-                                .read<MainScreenProvider>()
-                                .Categories[index]
-                                .iconCategory,
+                  expandedHeight: 59,
+                  title: Padding(
+                    padding: EdgeInsets.all(mainPadding),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        InkWell(
+                          onTap: () {},
+                          child: const Icon(
+                            Icons.search,
+                            color: Colors.grey,
                           ),
-                        );
-                      },
+                        ),
+                        Text(
+                          'Жалко выкинуть',
+                          style: Theme.of(context)
+                              .textTheme
+                              .titleLarge!
+                              .copyWith(fontSize: 25),
+                        ),
+                        InkWell(
+                          onTap: () {
+                            context.go('/main/account');
+                          },
+                          child: const Icon(
+                            Icons.manage_accounts_outlined,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                 ),
-              ),
-              SliverAppBar(
-                elevation: 0,
-                floating: true,
-                pinned: false,
-                snap: false,
-                titleSpacing: 0,
-                backgroundColor: Colors.white,
-                automaticallyImplyLeading: false,
-                expandedHeight: 20,
-                title: Center(
-                  child: Text(
-                    nameCategory,
-                    style: const TextStyle(color: Colors.black),
+                SliverPadding(
+                  padding: const EdgeInsets.only(bottom: 10),
+                  sliver: SliverAppBar(
+                    toolbarHeight: 120,
+                    elevation: 0,
+                    floating: true,
+                    pinned: false,
+                    snap: true,
+                    backgroundColor: Colors.white,
+                    automaticallyImplyLeading: false,
+                    titleSpacing: 0,
+                    title: Container(
+                      height: 100,
+                      child: ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: context
+                            .read<MainScreenProvider>()
+                            .Categories
+                            .length,
+                        itemBuilder: (context, index) {
+                          return InkWell(
+                            onTap: () {
+                              context.read<MainScreenProvider>().getAllDb(
+                                  context
+                                      .read<MainScreenProvider>()
+                                      .Categories[index]
+                                      .numCategory);
+
+                              context
+                                  .read<MainScreenProvider>()
+                                  .changeName(index);
+                              setState(() {});
+                            },
+                            child: CategoryBlock(
+                              index: context
+                                  .read<MainScreenProvider>()
+                                  .activeCategory,
+                              numCat: context
+                                  .read<MainScreenProvider>()
+                                  .Categories[index]
+                                  .numCategory,
+                              name: context
+                                  .read<MainScreenProvider>()
+                                  .Categories[index]
+                                  .nameCategory,
+                              icon: context
+                                  .read<MainScreenProvider>()
+                                  .Categories[index]
+                                  .iconCategory,
+                            ),
+                          );
+                        },
+                      ),
+                    ),
                   ),
                 ),
-              ),
-              SliverPadding(
-                padding: const EdgeInsets.symmetric(horizontal: 15),
-                sliver: SliverGrid(
-                  delegate: SliverChildBuilderDelegate(
-                    childCount: !dataIsLoaded ? 4 : productList.length,
-                    (context, index) {
-                      return ProductBlock(
-                          productList: productList,
-                          dataIsLoaded: dataIsLoaded,
-                          index: index);
-                    },
-                  ),
-                  gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-                    maxCrossAxisExtent: 200,
-                    mainAxisExtent: 210,
-                    crossAxisSpacing: 20,
-                    mainAxisSpacing: 15,
+                SliverAppBar(
+                  elevation: 0,
+                  floating: true,
+                  pinned: false,
+                  snap: false,
+                  titleSpacing: 0,
+                  backgroundColor: Colors.white,
+                  automaticallyImplyLeading: false,
+                  expandedHeight: 20,
+                  title: Center(
+                    child: Text(
+                      nameCategory,
+                      style: const TextStyle(color: Colors.black),
+                    ),
                   ),
                 ),
-              ),
-            ],
+                SliverPadding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15),
+                  sliver: SliverGrid(
+                    delegate: SliverChildBuilderDelegate(
+                      childCount: !dataIsLoaded ? 4 : productList.length,
+                      (context, index) {
+                        return ProductBlock(
+                            productList: productList,
+                            dataIsLoaded: dataIsLoaded,
+                            index: index);
+                      },
+                    ),
+                    gridDelegate:
+                        const SliverGridDelegateWithMaxCrossAxisExtent(
+                      maxCrossAxisExtent: 200,
+                      mainAxisExtent: 210,
+                      crossAxisSpacing: 20,
+                      mainAxisSpacing: 15,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
